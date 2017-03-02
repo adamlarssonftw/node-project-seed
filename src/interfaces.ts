27fd6c9db67bcfe747c4e602b01a45e69036bbe1
Adam <adamlarsson92@gmail.com>
2017-03-02 01:13:28 +0100

    Revert "Setup basic scraping/sorting functionality."
    This reverts commit 105a6219da7539a33a63b090011fc4207aac1544.

--- src/interfaces.ts
+++
@@ -1,9 +0,0 @@
-export interface IItem {
-    id?: string;
-    name?: string;
-    buy?: string;
-    sell?: string;
-    buy_currency?: string;
-    sell_currency?: string;
-    spread?: number;
-}
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,20 @@
 import { Observable, Observer } from "rxjs";
 import { RxHttpRequest } from "rx-http-request";
-import { Calculator } from "./calculator-service";
-import { Scraper } from "./scraper-service";
-import { IItem } from "./interfaces";
 
 class Main {
-  private goldItems: IItem[];
-  private silverItems: IItem[];
-  private platinumItems: IItem[];
-  private priceDataSourceURL = "http://www.degussa-goldhandel.de/infothek/preisliste/";
-  private scraper: Scraper;
+    private requestURL = "http://www.google.com";
+    constructor() {
+        this.attemptRequest(this.requestURL);
+    }
 
-  constructor() {
-    this.scraper = new Scraper();
-
-    this.scraper.attemptRequest(this.priceDataSourceURL).subscribe((html: string) => {
-      this.scraper.loadCheerioWithHtml(html);
-      this.goldItems = this.scraper.getItemsForSale("#tab2");
-      this.silverItems = this.scraper.getItemsForSale("#tab3");
-      // this.platinumItems = this.scraper.getItemsForSale("#tab4");
-      console.log(Calculator.sortBySpread(this.silverItems).slice(0, 5));
-      console.log(Calculator.sortBySpread(this.goldItems).slice(0, 5));
-    });
-  }
-
+    private attemptRequest(url: string) {
+        return RxHttpRequest.get(url).subscribe(
+            (response) => {
+                console.log(response);
+            },
+            (err) => console.log(err)
+        );
+    }
 }
 
 let m = new Main();
